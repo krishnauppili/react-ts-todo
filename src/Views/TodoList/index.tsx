f303d6fc6f60ea23905ff74c820fbd7c9183fe37
krishnauppili <krishna.uppili96@gmail.com>
2019-10-02 18:02:12 +0530

    Listing component developed and styles added

--- src/Views/TodoList/index.tsx
+++ src/Views/TodoList/index.tsx
@@ -1,16 +1,29 @@
 import * as React from 'react';
 import {Component} from "react";
 import TodoListContainer from "../../store/TodoList/container";
+import TodoList from "../../store/TodoList/models/todo_model";
+import "./styles.todoList.css";
 
 class TodoLists extends Component<any,any>{
+
     componentWillMount(): void {
         this.props.getAllTodoList();
     }
+
     render(){
-        console.log("Properties",this.props);
+        const {todoLists} = this.props;
         return(
-            <div>
-                Reached TodoList module
+            <div className="todo-lists">
+                <div style={{marginBottom:"1rem"}}>Todo lists</div>
+                {todoLists.map((todoListItem:TodoList,i:number)=>{
+                    return(
+                        <div key={i} className="todo-list-item">
+                            <div className="list-title">Title : {todoListItem.title}</div>
+                            <div>Description : {todoListItem.description}</div>
+                            <div>Task Total : {todoListItem.tasks.length}</div>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
