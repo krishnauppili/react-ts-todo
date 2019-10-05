40d567ce932f1eaebd398dfdd5c221098ac09ce5
krishnauppili <krishna.uppili96@gmail.com>
2019-10-05 15:04:11 +0530

    Added views for Update Task

--- src/Views/TodoList/index.tsx
+++ src/Views/TodoList/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {Component} from "react";
+import _ from "lodash";
 import TodoListContainer from "../../store/TodoList/container";
 import TodoList from "../../store/TodoList/models/todo_model";
 import "./styles.todoList.css";
@@ -11,17 +12,22 @@ class TodoLists extends Component<any,any>{
     }
 
     render(){
-        const {todoLists,currentList} = this.props;
-        console.log(todoLists,currentList);
+        const {todoLists,getCurrentTodoList} = this.props;
         return(
             <div className="todo-lists">
-                <div style={{marginBottom:"1rem"}}>Todo lists</div>
+                <div style={{marginBottom:"1rem"}}>
+                    <span>Todo lists</span>
+                    <span className="ml-1"><button>Add List</button></span>
+                </div>
                 {todoLists.map((todoListItem:TodoList,i:number)=>{
+                    const totalTasks = todoListItem.tasks.length;
+                    const completedTasks = _.filter(todoListItem.tasks,{isComplete:true}).length;
                     return(
-                        <div key={i} className="todo-list-item">
+                        <div key={i} className="todo-list-item"
+                             onClick={()=>getCurrentTodoList(todoLists,todoListItem.id)}>
                             <div className="list-title">Title : {todoListItem.title}</div>
                             <div>Description : {todoListItem.description}</div>
-                            <div>Task Total : {todoListItem.tasks.length}</div>
+                            <div>Task Total : {totalTasks} | Completed: {completedTasks}</div>
                         </div>
                     )
                 })}
